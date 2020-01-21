16cebcbfa2ba0412fde3a35991e7b62ac7eb5290
wired <tolyeg@gmail.com>
2020-01-21 20:59:10 +0200

    Initial commit

---
+++ src/com/wired/work11/Task11.java
@@ -0,0 +1,8 @@
+package com.wired.work11;
+
+public class Task11 {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello students!");
+    }
+}
---
+++ src/com/wired/work13/Task13.java
@@ -0,0 +1,27 @@
+package com.wired.work13;
+
+public class Task13 {
+    public static void main(String[] args) {
+        String letterJ =
+                  "  + + +\n"
+                + "      +\n"
+                + "      +\n"
+                + "+     +\n"
+                + " + + +\n";
+        String letterA =
+                  "   +\n"
+                + " +   +\n"
+                + "+ + + +\n"
+                + "+     +\n"
+                + "+     +\n";
+        String letterV =
+                  "+     +\n"
+                + "+     +\n"
+                + "+     +\n"
+                + " +   +\n"
+                + "   + \n";
+
+
+        System.out.println(letterJ+"\n"+letterA+"\n"+letterV+"\n"+letterA);
+    }
+}

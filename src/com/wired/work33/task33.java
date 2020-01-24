fb550d4f156ba2842fc05fed74f32422427bcf83
wired <tolyeg@gmail.com>
2020-01-24 20:26:00 +0200

    24-1-2020

---
+++ src/com/wired/work33/task33.java
@@ -0,0 +1,12 @@
+package com.wired.work33;
+
+public class task33 {
+    public static void main(String[] args) {
+        int sum = 1000;
+        double percD = 10.0;
+        double percU = 7;
+        double cost = sum - sum * percD / 100 ;
+        cost += cost * percU / 100;
+        System.out.println("cost =" + cost);
+    }
+}

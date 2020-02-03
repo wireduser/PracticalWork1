51ac4d74215538ed0ec20fe24dacf0d5047ea3ea
wired <tolyeg@gmail.com>
2020-02-04 00:12:07 +0200

    1-2-2020_3

--- src/com/wired/work72/task72.java
+++ src/com/wired/work72/task72.java
@@ -1,7 +1,18 @@
 package com.wired.work72;
 
+import java.util.Arrays;
+
 public class task72 {
     public static void main(String[] args) {
-
+        int [] array = {1,2,3,4,5,6,77,0};
+        System.out.println(sumOddElementsArray(array));
+    }
+    public static int sumOddElementsArray(int[] array){
+        int result = 0;
+        for (int digit : array) {
+            if (digit <0) return -1;
+            else if (digit % 2 > 0) result += digit;
+        }
+        return result;
     }
 }
